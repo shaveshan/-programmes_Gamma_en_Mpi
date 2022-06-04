fead1d1f80f58079767a4a7e02b8c9b905e9c8c8
shaveshan <55841152+shaveshan@users.noreply.github.com>
2022-06-04 18:11:13 +0100

    Update gamma.c

--- gamma.c
+++ gamma.c
@@ -7,18 +7,32 @@
 	éléments sont simples
 	Réaction R(x, y) = TRUE
 	Action   A(x, y) = x+y
+
+
+	!mpicc sum.c -o sum
+	!mpirun --allow-run-as-root -np 8 ./sum
 */
+
 
 #include <stdio.h>
 #include <unistd.h>
 #include "mpi.h"
 #include <stdlib.h>
 
-int N_threads = 8;
-int N = 16;
-int old_ms[16] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-int new_ms[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+#define N_threads 8
+#define N 14
+int old_ms[N] , new_ms[N] , perd_ms[N];
 int N_taches, N_taches_per_thread;
+int perd_rank = 0;
+
+
+void init_data(){
+	for(int iter=0; iter<N; iter++)  old_ms[iter]  = 4;
+	for(int iter=0; iter<N; iter++)  new_ms[iter]  = 0;
+	for(int iter=0; iter<N; iter++)  perd_ms[iter] = 0;
+}
+
+
 
 void actifs(int deb, int fin)
 {
@@ -26,12 +40,12 @@ void actifs(int deb, int fin)
 	MPI_Status status;
 	for (slave=deb; slave<fin; slave++)
 	{
-		MPI_Send(&old_ms[2*slave], 1, MPI_INT, slave,tag, MPI_COMM_WORLD);
-		MPI_Send(&old_ms[2*slave+1], 1, MPI_INT, slave,tag, MPI_COMM_WORLD);
-		//printf("Maitre sent to actif %d out of %d       // data %d %d\n", slave, fin, old_ms[2*slave], old_ms[2*slave+1]);
+		MPI_Send(&old_ms[2*slave], 1, MPI_INT, slave%N_threads,tag, MPI_COMM_WORLD);
+		MPI_Send(&old_ms[2*slave+1], 1, MPI_INT, slave%N_threads,tag, MPI_COMM_WORLD);
+		printf("Maitre sent to actif %d out of %d       // data %d %d\n", slave, fin, old_ms[2*slave], old_ms[2*slave+1]);
 			
-		MPI_Recv(&new_ms[slave], 1, MPI_INT, slave, tag, MPI_COMM_WORLD, &status);
-		//printf("Maitre received from actif %d out of %d // data %d\n", slave, fin, new_ms[slave]);
+		MPI_Recv(&new_ms[slave], 1, MPI_INT, slave%N_threads, tag, MPI_COMM_WORLD, &status);
+		printf("Maitre received from actif %d out of %d // data %d\n", slave, fin, new_ms[slave]);
 	}
 }
 
@@ -42,12 +56,12 @@ void passifs(int deb, int fin)
 	int dummy = 0;
 	for (slave=deb; slave<fin; slave++)
 	{
-		MPI_Send(&dummy, 1, MPI_INT, slave,tag, MPI_COMM_WORLD);
-		MPI_Send(&dummy, 1, MPI_INT, slave,tag, MPI_COMM_WORLD);
-		//printf("Maitre sent to passif %d out of %d       // data %d %d\n", slave, fin, dummy, dummy);
+		MPI_Send(&dummy, 1, MPI_INT, slave%N_threads,tag, MPI_COMM_WORLD);
+		MPI_Send(&dummy, 1, MPI_INT, slave%N_threads,tag, MPI_COMM_WORLD);
+		printf("Maitre sent to passif %d out of %d       // data %d %d\n", slave, fin, dummy, dummy);
 			
-		MPI_Recv(&dummy, 1, MPI_INT, slave, tag, MPI_COMM_WORLD, &status);
-		//printf("Maitre received from passif %d out of %d // data %d\n", slave, fin, dummy);
+		MPI_Recv(&dummy, 1, MPI_INT, slave%N_threads, tag, MPI_COMM_WORLD, &status);
+		printf("Maitre received from passif %d out of %d // data %d\n", slave, fin, dummy);
 	}
 }
 
@@ -72,19 +86,30 @@ void action (void)
 }
 
 //====================================================fonction de l'exclave==========================================
+
+int get_cycle( int lendata){
+	int cycle = 0 ,p = 2;
+
+	while( p < lendata ){
+		p*=2;
+		cycle++;
+	}
+	return p==lendata ? cycle : cycle-1;
+}
+
 void worker(void)
 {
-	int iter1, iter2, cycle = 3;
+	int  cycle = get_cycle(N);
 	// cas ou le nombre de taches est supérieur au nombre de threads
 	if (N_taches > N_threads)
 	{
-		for (iter1 = 1; iter1 < N_taches_per_thread; iter1++)
+		for (int iter1 = 1; iter1 < N_taches_per_thread; iter1++)
 		{ 
 			action();
 		}
 	}else
 		// cas ou le nombre de taches est égal ou inférieur au  nombre de threads
-		for (iter2 = 0; iter2 < cycle; iter2++)
+		for (int iter2 = 0; iter2 < cycle; iter2++)
 		{
 			action();
 		}
@@ -93,7 +118,7 @@ void worker(void)
 //====================================================fonction du maitre==========================================
 void master_as_dispacher(void)
 {
-	int i, iter, cycle = 3, slave;
+	int i, iter, slave;
 	int tag = 50;
 	MPI_Status status;
 
@@ -106,9 +131,16 @@ void master_as_dispacher(void)
 			N_taches_per_thread = N_taches / N_threads;
 			for (iter=0; iter<N_taches_per_thread; iter++)
 			{
-				master_as_worker(iter*N_taches_per_thread, iter*N_taches_per_thread + 1);
-				actifs(iter*N_taches_per_thread + 1, N_threads);
+				master_as_worker(iter*N_threads, iter*N_threads + 1);
+				actifs(iter*N_threads + 1, N_threads*(iter+1));
 			}
+			if (N_taches%N_threads != 0) passifs(N_threads*(iter+1), N_taches);
+
+			if(N_taches % 2 == 1 ){
+				perd_ms[perd_rank] =  new_ms[N_taches - 1];
+				perd_rank++;
+			}
+
 			N_taches = N_taches/2;
 			N_taches_per_thread = N_taches_per_thread/2;
 		}
@@ -116,29 +148,67 @@ void master_as_dispacher(void)
 	// Cas ou le nombre de taches est égal ou inférieur au nombre de threads
 	if (N_taches <= N_threads)
 	{	
+		int p = 0;
 		while (N_taches > 1)		
 		{	
-			if (N_taches ==	N_threads)
-			{
-				master_as_worker(0, 1);
-				actifs(1, N_taches);
-			}else
-			{
-				master_as_worker(0, 1);
-				actifs(1, N_taches);
-				passifs(N_taches, N_threads);
-			}
+			
+
+			master_as_worker(0, 1);
+			actifs(1, N_taches);
+			if (N_taches !=	N_threads) passifs(N_taches, N_threads);
+
+
+
 			for(iter=0; iter<N_taches; iter++) old_ms[iter] = new_ms[iter];
 			printf("\n When N_taches = %d new_ms = ", N_taches);
 			for(iter=0; iter<N_taches; iter++) printf(" %d ", old_ms[iter]);
-			N_taches = N_taches/2;				
+			
+
+
+			if(N_taches % 2 == 1 ){
+				perd_ms[perd_rank] =  new_ms[N_taches - 1];
+				perd_rank++;
+			}
+
+
+			N_taches = N_taches/2;
+						
 		}
 		// Dernier tache est prise par le maitre
-		master_as_worker(0, 1);	
+		master_as_worker(0, 1);
+
+
+	
 		for(iter=0; iter<N_taches; iter++) old_ms[iter] = new_ms[iter];
-		printf("\n When N_taches = %d new_ms = ", N_taches);
+		printf("\n When N_taches = %d new_ms  = ", N_taches);
 		for(iter=0; iter<N_taches; iter++) printf(" %d ", old_ms[iter]);
 		printf("\n");
+
+		
+
+		
+
+
+		
+
+		
+	}
+
+	if( perd_rank > 0 ){
+
+		perd_ms[perd_rank] = old_ms[0]; 
+		N_taches = (perd_rank + 1)/2;
+		for(iter=0; iter<perd_rank+1; iter++) old_ms[iter] = perd_ms[iter];
+		printf("\n --------> perd_rank = %d  ----- perd_ms = ", perd_rank);
+		for(iter=0; iter<perd_rank+1; iter++)  printf(" %d ", perd_ms[iter]);
+		printf("\n");
+		if( perd_rank % 2 == 1){
+			perd_rank = 0;
+		}else{
+			perd_ms[0] = old_ms[perd_rank];
+			perd_rank = 1;
+		}
+		master_as_dispacher();
 	}		
 	// Afficher le résultatS	
 	//printf("Résult = %d\n", old_ms[0]);
@@ -160,16 +230,25 @@ void Afficher_Info(void)
 	printf("===================================================================\n");
 }
 
+
+
+
 //=======================================Programme principal=================================================
 int main(int argc, char** argv) 
 {
+	init_data();
 	int rank; 			/* Rank of process */
 	int tag = 50;		/* Tag for messages */
 	// int multiset[16] = {10, 4, 71, 15, 21, 14, 51, 19, 1, 9, 32, 17, 12, 4, 61, 8};
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	
+
+	if(N % 2 == 1){
+		perd_ms[perd_rank] =  old_ms[N - 1];
+		perd_rank++;
+	}
+
 	N_taches = N/2;
 	N_taches_per_thread = N_taches/N_threads;
 		
